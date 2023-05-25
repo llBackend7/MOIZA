0ec0576d535e2931e46fa82e82dcbd3eb53ac012
Jzakka <mouse4786@gmail.com>
2023-05-25 15:21:30 +0900

    Feat: 시간검증

--- src/main/java/com/ll/MOIZA/boundedContext/room/service/RoomService.java
+++ src/main/java/com/ll/MOIZA/boundedContext/room/service/RoomService.java
@@ -4,8 +4,10 @@ import com.ll.MOIZA.boundedContext.member.entity.Member;
 import com.ll.MOIZA.boundedContext.room.entity.Room;
 import com.ll.MOIZA.boundedContext.room.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -28,6 +30,8 @@ public class RoomService {
                            LocalTime availableEndTime,
                            LocalTime duration,
                            LocalDateTime deadLine) {
+        validateTimes(startDay, endDay, availableStartTime, availableEndTime, deadLine);
+
         Room room = Room.builder()
                 .leader(member)
                 .name(name)
@@ -43,5 +47,20 @@ public class RoomService {
         Room createdRoom = roomRepository.save(room);
 
         return createdRoom;
+    }
+
+    private void validateTimes(LocalDate startDay, LocalDate endDay, LocalTime availableStartTime, LocalTime availableEndTime, LocalDateTime deadLine) {
+        if (!startDay.isBefore(endDay)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "가능날짜가 잘못되었습니다.");
+        }
+        if (!availableStartTime.isBefore(availableEndTime)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "가능시간이 잘못되었습니다.");
+        }
+        if (!deadLine.isAfter(LocalDateTime.now())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "마감시간이 잘못되었습니다.");
+        }
+        if (!deadLine.isBefore(startDay.atStartOfDay())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "마감시간은 가능한 날짜보다 이전이어야 합니다.");
+        }
     }
 }
