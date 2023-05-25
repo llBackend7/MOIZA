39f2f2aecfe06da9afd791b797f26016a7edb3b6
youngkyunIm <yee0230@naver.com>
2023-05-25 17:12:45 +0900

    Feat: 장소 선택 및 테스트

--- src/test/java/com/ll/MOIZA/boundedContext/selectedPlace/service/SelectedPlaceServiceTest.java
+++ src/test/java/com/ll/MOIZA/boundedContext/selectedPlace/service/SelectedPlaceServiceTest.java
@@ -1,4 +1,73 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.ll.MOIZA.boundedContext.selectedPlace.service;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.ll.MOIZA.boundedContext.member.entity.Member;
+import com.ll.MOIZA.boundedContext.member.repository.MemberRepository;
+import com.ll.MOIZA.boundedContext.room.entity.EnterRoom;
+import com.ll.MOIZA.boundedContext.room.entity.Room;
+import com.ll.MOIZA.boundedContext.room.repository.EnterRoomRepository;
+import com.ll.MOIZA.boundedContext.room.repository.RoomRepository;
+import com.ll.MOIZA.boundedContext.room.service.EnterRoomService;
+import com.ll.MOIZA.boundedContext.room.service.RoomService;
+import com.ll.MOIZA.boundedContext.selectedPlace.entity.SelectedPlace;
+import com.ll.MOIZA.boundedContext.selectedPlace.repository.SelectedPlaceRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
 class SelectedPlaceServiceTest {
-  
+
+    @Autowired
+    SelectedPlaceRepository selectedPlaceRepository;
+
+    @Autowired
+    SelectedPlaceService selectedPlaceService;
+
+    @Autowired
+    EnterRoomService enterRoomService;
+    @Autowired
+    RoomService roomService;
+
+    @Autowired
+    EnterRoomRepository enterRoomRepository;
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void 장소_선택() {
+        Member member = memberRepository.findByName("user1").get();
+        Room room = roomService.createRoom(
+                member,
+                "테스트룸",
+                "테스트룸임",
+                LocalDate.now().plusDays(5),
+                LocalDate.now().plusDays(7),
+                LocalTime.of(1, 0),
+                LocalTime.of(5, 0),
+                LocalTime.of(3, 0),
+                LocalDateTime.now().plusDays(2));
+
+        EnterRoom enterRoom = enterRoomService.createEnterRoom(room, member);
+
+        SelectedPlace selectedPlace = selectedPlaceService.CreateSelectedPlace("테스트 장소", enterRoom);
+
+        assertAll(
+                () -> assertThat(selectedPlace.getName()).isEqualTo("테스트 장소"),
+                () -> assertThat(selectedPlace.getEnterRoom().getId()).isEqualTo(enterRoom.getId())
+        );
+    }
 }
