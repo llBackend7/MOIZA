8f23dd1839f34feab57232dd64ef49c3ee1a127b
youngkyunIm <yee0230@naver.com>
2023-05-25 17:08:18 +0900

    Feat: 방 입장 및 테스트

--- src/main/java/com/ll/MOIZA/boundedContext/selectedTime/entity/SelectedTime.java
+++ src/main/java/com/ll/MOIZA/boundedContext/selectedTime/entity/SelectedTime.java
@@ -4,23 +4,23 @@ import com.ll.MOIZA.base.entity.BaseEntity;
 import com.ll.MOIZA.boundedContext.room.entity.EnterRoom;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import java.time.LocalTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor
 @SuperBuilder
 @ToString(callSuper = true)
-public class SelectedTime extends BaseEntity {
+public class SelectedTimes extends BaseEntity {
     private LocalDate day;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private LocalTime startTime;
+    private LocalTime endTime;
     @ManyToOne
     private EnterRoom enterRoom;
 }
