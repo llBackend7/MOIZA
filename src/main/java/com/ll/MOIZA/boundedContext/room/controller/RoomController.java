0e006472e82db4aa20f00f674b75707b32c6d306
Jzakka <mouse4786@gmail.com>
2023-05-25 14:07:46 +0900

    Chore: 방생성 준비
    - 컨트롤러 생성
    - 뷰화면 생성

---
+++ src/main/java/com/ll/MOIZA/boundedContext/room/controller/RoomController.java
@@ -0,0 +1,11 @@
+package com.ll.MOIZA.boundedContext.room.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/room")
+public class RoomController {
+}
