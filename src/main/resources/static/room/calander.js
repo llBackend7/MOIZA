40f92a11795241337a2e56fd89bbe0602c996a5f
Jzakka <mouse4786@gmail.com>
2023-05-27 12:42:43 +0900

    Feat: 프론트에서 입력 검증

--- src/main/resources/static/room/calander.js
+++ src/main/resources/static/room/calander.js
@@ -17,3 +17,57 @@ $(document).ready(function() {
         }
     }
 });
+
+$(document).ready(function() {
+    $('form').on('submit', function(e) {
+        e.preventDefault(); // 폼 제출의 기본 동작을 방지합니다.
+
+        let  startDate = $('#startDate').val();
+        let  endDate = $('#endDate').val();
+        let  name = $('#name').val();
+        let  description = $('#description').val();
+        let  place = $('#place').val();
+        let  duration = $('#duration').val();
+        let  startTime = $('#startTime').val();
+        let  endTime = $('#endTime').val();
+        let  deadLine = $('#deadLine').val();
+
+        validateTimes(startDate, endDate, name, description, place, duration, startTime, endTime, deadLine);
+
+        // console.log('Start Date: ', startDate);
+        // console.log('End Date: ', endDate);
+        // console.log('Name: ', name);
+        // console.log('Description: ', description);
+        // console.log('Place: ', place);
+        // console.log('Duration: ', duration);
+        // console.log('Start Time: ', startTime);
+        // console.log('End Time: ', endTime);
+        // console.log('Dead Line: ', deadLine);
+    });
+});
+
+function validateTimes(startDate, endDate, name, description, place, duration, startTime, endTime, deadLine) {
+    startDate = new Date(startDate);
+    endDate = new Date(endDate);
+    startTime = new Date(`1970-01-01T${startTime}:00`);
+    endTime = new Date(`1970-01-01T${endTime}:00`);
+    deadLine = new Date(deadLine);
+
+    if (endDate < startDate) {
+        alert("가능날짜가 잘못되었습니다.");
+        return false;
+    }
+    if (endTime < startTime) {
+        alert("가능시간이 잘못되었습니다.");
+        return false;
+    }
+    if (new Date() > deadLine) {
+        alert("마감시간이 잘못되었습니다.");
+        return false;
+    }
+    if (startDate < deadLine) {
+        alert("마감시간은 가능한 날짜보다 이전이어야 합니다.");
+        return false;
+    }
+    return true;
+}
