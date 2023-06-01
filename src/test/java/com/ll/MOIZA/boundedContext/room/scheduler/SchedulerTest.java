0747d5abf8239aa67744a54f84e47f7167fdc0eb
Jzakka <mouse4786@gmail.com>
2023-06-01 14:52:20 +0900

    Chore: 메일 발신 주소 변경

--- src/test/java/com/ll/MOIZA/boundedContext/room/scheduler/SchedulerTest.java
+++ src/test/java/com/ll/MOIZA/boundedContext/room/scheduler/SchedulerTest.java
@@ -5,7 +5,6 @@ import com.ll.MOIZA.boundedContext.member.entity.Member;
 import com.ll.MOIZA.boundedContext.room.entity.EnterRoom;
 import com.ll.MOIZA.boundedContext.room.entity.Room;
 import com.ll.MOIZA.boundedContext.room.repository.RoomRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -14,7 +13,7 @@ import java.lang.reflect.Field;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SchedulerTest {
     List<Room> roomToMailSend;
