5d315959a712f11c5d670cbc4ba9e92e5408c8ad
LEH96 <lutea67@naver.com>
2023-06-13 13:20:39 +0900

    Feat: 모임 정렬 순서 변경(최신순-이름순으로)

--- src/main/java/com/ll/MOIZA/boundedContext/home/controller/HomeController.java
+++ src/main/java/com/ll/MOIZA/boundedContext/home/controller/HomeController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -51,6 +52,7 @@ public class HomeController {
 
         List<Room> rooms = rq.getMember().getEnterRooms().stream()
                 .map(EnterRoom::getRoom)
+                .sorted(Comparator.comparing(Room::getCreateDate).reversed().thenComparing(Room::getName))
                 .collect(Collectors.toList());
 
         model.addAttribute("member", rq.getMember());
